b4230f27c697cda513b48011c68c6ae0e389b69c
huangchaofeng <961195916@qq.com>
2016-09-03 19:37:18 +0800

    完成了在小组页面上显示热门小组的功能      --hcf

--- newhaozhiwang/src/main/java/com/haozhi/service/impl/CgroupServiceImpl.java
+++ newhaozhiwang/src/main/java/com/haozhi/service/impl/CgroupServiceImpl.java
@@ -1,10 +1,23 @@
 package com.haozhi.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.haozhi.entity.Cgroup;
+import com.haozhi.mapper.CgroupMapper;
 import com.haozhi.service.CgroupService;
 
 @Service("cgroupService")
 public class CgroupServiceImpl implements CgroupService{
+	
+	@Autowired
+	private CgroupMapper groupMapper;
+	@Override
+	public List<Cgroup> getHostGroups() {
+		List<Cgroup> groups=groupMapper.getHostGroups();
+		return groups;
+	}
 
 }
