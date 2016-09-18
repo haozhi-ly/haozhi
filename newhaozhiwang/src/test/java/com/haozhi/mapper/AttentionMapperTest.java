39178a55816548c9df5fb92efe883a5facfdda4a
huangchaofeng <961195916@qq.com>
2016-09-18 20:54:33 +0800

    在person.jsp界面完善了一些小细节     --hcf

---
+++ newhaozhiwang/src/test/java/com/haozhi/mapper/AttentionMapperTest.java
@@ -0,0 +1,25 @@
+package com.haozhi.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring.xml")
+public class AttentionMapperTest {
+	
+	@Autowired
+	private AttentionMapper attentionMapper;
+	@Test
+	public void testGetAttentionInfo() {
+		System.out.println(attentionMapper.getAttentionInfo("41"));
+	}
+	
+	@Test
+	public void testGetAttentionCount() {
+		System.out.println(attentionMapper.getAttentionCount("41"));
+	}
+
+}
